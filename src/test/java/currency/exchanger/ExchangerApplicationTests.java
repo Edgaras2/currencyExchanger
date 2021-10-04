726ecca8cb34efcc1312a7a36b675098ccf1b688
Edgaras <edgarasbaj@gmail.com>
2021-10-04 14:23:12 +0300

    init

---
+++ src/test/java/currency/exchanger/ExchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package currency.exchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
